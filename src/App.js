58ae5dbd4b3e37f81562513aaa42007b10d0bd84
devalencia12 <devalencia@bancodeoccidente.com.co>
2022-09-09 17:56:11 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import KanbanBoard from "./KanbanBoard.js";
+
+function App() {
+  return (
+    <div className="App">
+      <KanbanBoard />
+    </div>
+  );
+}
+
+export default App;
